d426b5e052493274f4edafb695f838312ab2ac54
Paul Sexton <github@psexton.net>
2025-03-30 16:58:57 -0500

    Add LogMetadata type

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,11 +1,16 @@
-export function log(level: 'info' | 'warn' | 'error', message: string, metadata?: Record<string, unknown>) {
-    const logEntry = {
+export type LogMetadata = {
+    [key: string]: unknown; // Allow any key with any value
+  };
+
+export function log(level: 'info' | 'warn' | 'error', message: string, metadata?: LogMetadata) {
+    const logEntry: Record<string, unknown> = {
       timestamp: new Date().toISOString(),
       level,
       message,
-      ...metadata, // Spread metadata into the log entry
+      ...metadata, // Spread metadata into the log entry. Supports both flat & nested metadata.
     };
   
-    console.log(JSON.stringify(logEntry));
+    // console.log(JSON.stringify(logEntry)); // Compact print for splunk/etc.
+    console.log(JSON.stringify(logEntry, null, 2)); // Pretty-print logs for readability
   }
   
