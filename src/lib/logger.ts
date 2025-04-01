a98f5898216ce13be39f3f9b278044e7b8f2d3dc
Paul Sexton <github@psexton.net>
2025-04-01 13:16:13 -0500

    log runtime. pretty print only when in dev mode.

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -7,10 +7,13 @@ export function log(level: 'info' | 'warn' | 'error', message: string, metadata?
       timestamp: new Date().toISOString(),
       level,
       message,
+      runtime: process.env.NEXT_RUNTIME,
       ...metadata, // Spread metadata into the log entry. Supports both flat & nested metadata.
     };
   
-    // console.log(JSON.stringify(logEntry)); // Compact print for splunk/etc.
-    console.log(JSON.stringify(logEntry, null, 2)); // Pretty-print logs for readability
+    if (process.env.NODE_ENV == 'development')
+        console.log(JSON.stringify(logEntry, null, 2)); // Pretty-print logs for readability
+    else
+        console.log(JSON.stringify(logEntry)); // Compact print for splunk/etc.
   }
   
