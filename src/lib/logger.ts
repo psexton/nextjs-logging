9951b220479bad9d29955614fc5036f280796197
Paul Sexton <github@psexton.net>
2025-04-02 12:28:00 -0500

    Remove hostname & pid, add runtime

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,15 +1,23 @@
 import pino, { Logger } from "pino";
 
-export const logger: Logger = 
-    pino({ 
+export const logger: Logger =
+    pino({
+        formatters: {
+            // Display log level by name instead of numeric value
+            level: (label) => {
+                return { level: label.toUpperCase() };
+            },
+            // Omit the hostname & pid fields that pino includes by default,
+            // and include the runtime instead
+            bindings: (bindings) => {
+                return {
+                    runtime: process.env.NEXT_RUNTIME,
+                };
+            },
+        },
         // Sets minimum level to output logs for
         level: process.env.PINO_LOG_LEVEL || "debug",
-        // Display log level by name instead of numeric value
-        formatters: {
-            level: (label) => {
-              return { level: label.toUpperCase() };
-            },
-          },
         // Use ISO8601 timestamp instead of unix epoch, and rename from "time" to "timestamp"
         timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
+
     });
