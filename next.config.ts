259fdaeff09fa1d851d17c9fe35f183703585c4a
Paul Sexton <github@psexton.net>
2025-04-03 16:01:12 -0500

    clean up dependencies & next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  serverExternalPackages: ["pino", "pino-pretty"],
 };
 
 export default nextConfig;
