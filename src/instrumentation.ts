8ea230965fa882443cadbbfd3b11f3f8e604ff66
Paul Sexton <github@psexton.net>
2025-04-04 13:21:58 -0500

    Add next-logger dependency

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,10 @@
 import { registerOTel } from '@vercel/otel'
  
-export function register() {
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await require('pino')
+    await require('next-logger')
+  }
+
   registerOTel({ serviceName: 'next-logging' })
 }
