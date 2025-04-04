e87bd163b47886d0daacea7782d9409556426e71
Paul Sexton <github@psexton.net>
2025-04-04 14:01:09 -0500

    add custom logger config. mostly works.

---
+++ next-logger.config.js
@@ -0,0 +1,63 @@
+const pino = require('pino');
+const { trace, context } = require('@opentelemetry/api');
+
+const nodePinoConfig = {
+    formatters: {
+        // Display log level by name instead of numeric value
+        level: (label) => {
+            return { level: label.toUpperCase() };
+        },
+        // Omit the hostname & pid fields that pino includes by default,
+        // and include the runtime instead
+        bindings: () => {
+            return {
+                runtime: process.env.NEXT_RUNTIME,
+            };
+        },
+    },
+    // Sets minimum level to output logs for
+    level: process.env.PINO_LOG_LEVEL || "debug",
+    // Use ISO8601 timestamp instead of unix epoch
+    timestamp: pino.stdTimeFunctions.isoTime,
+    mixin: () => {
+        const activeSpan = trace.getSpan(context.active());
+        const traceId = activeSpan?.spanContext().traceId ?? "unknown";
+        const spanId = activeSpan?.spanContext().spanId ?? "unknown";
+        return { traceId, spanId, }
+    },
+}
+
+const edgePinoConfig = {
+    browser: {
+        asObject: true,
+        formatters: {
+            // Display log level by name instead of numeric value
+            level: (label) => {
+                return { level: label.toUpperCase() };
+            },
+        },
+        write: (logEntry) => { 
+            // Take in the log obj and recast it to a Record so we can add fields to it,
+            // replicating what we do with bindings in the nodeRuntimeConfig
+            const activeSpan = trace.getSpan(context.active());
+            const traceId = activeSpan?.spanContext().traceId ?? "unknown";
+            const spanId = activeSpan?.spanContext().spanId ?? "unknown";
+
+            logEntry.runtime = process.env.NEXT_RUNTIME ?? "unknown"
+            logEntry.traceId = traceId
+            logEntry.spanId = spanId
+            console.log(JSON.stringify(logEntry)) 
+        },
+    },
+}
+
+// Pino instance used by NextJS, via next-logging
+const logger = (defaultConfig) =>
+    pino({
+        ...defaultConfig,
+        ...(process.env.NEXT_RUNTIME === 'edge' ? edgePinoConfig : nodePinoConfig),
+    })
+
+module.exports = {
+    logger,
+}
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,8 +2,8 @@ import { registerOTel } from '@vercel/otel'
  
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await require('pino')
-    await require('next-logger')
+    await require('pino')         // eslint-disable-line @typescript-eslint/no-require-imports
+    await require('next-logger')  // eslint-disable-line @typescript-eslint/no-require-imports
   }
 
   registerOTel({ serviceName: 'next-logging' })
